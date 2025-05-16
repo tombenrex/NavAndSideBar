76ec21bcc99227016112639fb1f9303eb189640a
Tom Lrsson <tmlsn@hotmail.com>
2025-05-16 14:57:49 +0200

    404 update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/NavAndSideBar/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        notfound: 'public/404.html',
+      },
+    },
+  },
 });
