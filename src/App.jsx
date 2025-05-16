d6102e19dfabc94d27626ab6484b2798cd25406d
Tom Lrsson <tmlsn@hotmail.com>
2025-05-16 14:34:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/header/NavBar';
+import Sidebar from './components/sidebar/SideBar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import TaskTwo from './components/pages/Task2';
+import TaskThree from './components/pages/Task3';
+import './App.css';
+
+function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <Router>
+      <Navbar
+        onSidebarOpen={() => setIsSidebarOpen((prev) => !prev)} // Toggle function
+        isSidebarOpen={isSidebarOpen}
+      />
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+      <div className='container mt-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/tasktwo' element={<TaskTwo />} />
+          <Route path='/taskthree' element={<TaskThree />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
